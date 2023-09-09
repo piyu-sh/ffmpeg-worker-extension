153686360769f592a5aae4251388f23e9b54d8fb
piyush gupta <piyush_g@outlook.com>
2023-09-09 16:28:15 +0530

    working ffmpeg.wasm in mv3 extension

--- src/pages/Newtab/Newtab.jsx
+++ src/pages/Newtab/Newtab.jsx
@@ -3,15 +3,56 @@ import logo from '../../assets/img/logo.svg';
 import './Newtab.css';
 import './Newtab.scss';
 
+window.onmessage = function (e) {
+  if (e.data.type == 'msg') {
+    const fixedData = e.data.data;
+    const fixedBlob = new Blob([fixedData.buffer], { type: 'video/webm' });
+    const blobUrl = URL.createObjectURL(fixedBlob);
+    chrome.downloads.download(
+      {
+        url: blobUrl,
+        filename: 'out.webm',
+      },
+      downloadId => {
+        if (downloadId) {
+          URL.revokeObjectURL(blobUrl);
+        } else {
+          console.error('🚀 ~ download failed');
+        }
+      }
+    );
+  }
+};
+
 const Newtab = () => {
+
+  async function fileHandler() {
+    const [fileHandle] = await window.showOpenFilePicker();
+    //@ts-ignore
+    const file = await fileHandle.getFile();
+    const fileAB = await file.arrayBuffer();
+
+    const ffmpegIframe = document.getElementById(
+      'theFrame'
+    );
+    ffmpegIframe.contentWindow.postMessage(
+      {
+        type: 'msg',
+        data: fileAB,
+      },
+      '*'
+    );
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <p>
           This is the main page
         </p>
-
       </header>
+      <button onClick={fileHandler}>load file to transcode</button>
     </div>
   );
 };
